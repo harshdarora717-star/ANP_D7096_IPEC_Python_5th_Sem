96f942cbb347653214925020f2b6ac8f63f27eb6
harshdarora717-star <harshdarora717@gmail.com>
2026-07-01 10:24:09 +0530

    using pop

--- classwork/Day6_01_july_list/deletion.py
+++ classwork/Day6_01_july_list/deletion.py
@@ -1,5 +1,7 @@
 #deletion from the list
 my_list = [1,2,3,4,5,6,7,8,9,10]
 index_to_delete = int(input("Enter the index of the element you want to delete: "))
-del my_list[index_to_delete]  
+my_list.pop(index_to_delete)
 print(my_list)
+#del my_list[index_to_delete]  
+#print(my_list)
